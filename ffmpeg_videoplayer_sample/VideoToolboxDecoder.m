7ca815122ec8f368ecdd6135be2dddecb6e440f2
HanGyo Jeong <>
2019-11-03 08:23:48 +0900

    VideoToolbox Make CreateVideoToolboxDecoder function

--- ffmpeg_videoplayer_sample/VideoToolboxDecoder.m
+++ ffmpeg_videoplayer_sample/VideoToolboxDecoder.m
@@ -18,12 +18,29 @@
     self = [super init];
     if(self){
         codecpar = get_codec_parameters();
-        
+        [self createVideoToolboxDecoder];
     }
     return self;
 }
 
+- (int)decodeVideo:(CVPixelBufferRef *)pixelBuffer{
+    int err = 0;
+    NAL_UNIT nal_unit = {NULL, 0};
+    err = get_video_packet(&nal_unit);
+}
+
 #pragma mark - VideoToolbox Activity
+static void didDecompress(void *decompressionOutputRefCon,
+                          void *sourceFrameRefCon,
+                          OSStatus status,
+                          VTDecodeInfoFlags infoFlags,
+                          CVImageBufferRef pixelBuffer,
+                          CMTime presentationTimeStamp,
+                          CMTime presentationDuration){
+    CVPixelBufferRef *outputPixelBuffer = (CVPixelBufferRef *)sourceFrameRefCon;
+    *outputPixelBuffer = CVPixelBufferRetain(pixelBuffer);
+}
+
 - (int)createVideoToolboxDecoder{
     /*
      [width & height]
@@ -62,6 +79,57 @@
      */
     dict_set_i32(par, CFSTR("HorizontalSpacing"), 0);
     dict_set_i32(par, CFSTR("VerticalSpacing"), 0);
+    
+    /*SampleDescriptionExtensionAtoms dict*/
+    dict_set_data(atoms, CFSTR("avcC"), (uint8_t*)extradata, extradata_size);
+    
+    /*Extensions dict*/
+    dict_set_string(extensions, CFSTR("CVImageBufferChromaLocationBottomField"), "left");
+    dict_set_string(extensions, CFSTR("CVImageBufferChromaLocationTopField"), "left");
+    dict_set_boolean(extensions, CFSTR("FullRangeVideo"), FALSE);
+    dict_set_object(extensions, CFSTR("CVPixelAspectRatio"), (CFTypeRef *)par);
+    dict_set_object(extensions, CFSTR("SampleDescriptionExtensionAtoms"), (CFTypeRef *)atoms);
+    
+    status = CMVideoFormatDescriptionCreate(NULL,
+                                            kCMVideoCodecType_H264,
+                                            width,
+                                            height,
+                                            extensions,
+                                            &(formatDescription));
+    
+    CFRelease(extensions);
+    CFRelease(atoms);
+    CFRelease(par);
+    
+    if(status != 0){
+        NSError *error = [NSError errorWithDomain:NSOSStatusErrorDomain code:status userInfo:nil];
+        NSLog(@"Error: creating format description failed. Description: %@", [error description]);
+        return -1;
+    }
+    
+    CFMutableDictionaryRef destinationPixelBufferAttributes;
+    destinationPixelBufferAttributes = CFDictionaryCreateMutable(NULL, 0, &kCFTypeDictionaryKeyCallBacks, &kCFTypeDictionaryValueCallBacks);
+    dict_set_i32(destinationPixelBufferAttributes, kCVPixelBufferPixelFormatTypeKey, kCVPixelFormatType_420YpCbCr8BiPlanarVideoRange);
+    dict_set_i32(destinationPixelBufferAttributes, kCVPixelBufferWidthKey, width);
+    dict_set_i32(destinationPixelBufferAttributes, kCVPixelBufferHeightKey, height);
+    dict_set_boolean(destinationPixelBufferAttributes, kCVPixelBufferOpenGLESCompatibilityKey, YES);
+    
+    /*VTDecompressionOutputCallbackRecord is a simple structure with a pointer to the callback function invoked when frame decompression */
+    VTDecompressionOutputCallbackRecord outputCallback;
+    outputCallback.decompressionOutputCallback = didDecompress;
+    outputCallback.decompressionOutputRefCon = NULL;
+    status = VTDecompressionSessionCreate(kCFAllocatorDefault,
+                                          formatDescription,
+                                          NULL,
+                                          destinationPixelBufferAttributes,
+                                          &outputCallback,
+                                          &(decompressSession));
+    if(status != noErr){
+        NSError *error = [NSError errorWithDomain:NSOSStatusErrorDomain code:status userInfo:nil];
+        NSLog(@"Error: Creating decompression session failed. Description: %@", [error description]);
+        return -1;
+    }
+    return 0;
 }
 
 #pragma mark - Utils
