a0d2bde65c87aa5ce50a11cd6192188863a91bd4
HanGyo Jeong <>
2019-11-03 09:30:53 +0900

    Add UIViewController which playing h.264 video frame

---
+++ ffmpeg_videoplayer_sample/DecodingViewController.h
@@ -0,0 +1,17 @@
+//
+//  DecodingViewController.h
+//  ffmpeg_videoplayer_sample
+//
+//  Created by HanGyo Jeong on 03/11/2019.
+//  Copyright © 2019 HanGyoJeong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface DecodingViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
